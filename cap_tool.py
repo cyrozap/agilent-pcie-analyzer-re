e6f07e95a2f565129ee2ed7cdcfaae933d4d5912
cyrozap <cyrozap@gmail.com>
2023-02-02 00:23:29 -0600

    Remove unused parameter

--- cap_tool.py
+++ cap_tool.py
@@ -131,7 +131,7 @@ def main():
             debug_data = " (unk0: 0x{:08x}, unk1: 0x{:08x}, unk2: 0x{:08x}, unk3: {}, bytes_valid: {} ({}), flags: 0x{:08x}, data_offset: {})".format(
                 record.unk0, record.unk1, record.unk2, record.unk3.hex(),
                 bytes_valid, bytes_valid_flag, record.flags,
-                record.data_offset, valid_data.hex())
+                record.data_offset)
 
         dllp = None
         if args.dllp or args.tlp:
