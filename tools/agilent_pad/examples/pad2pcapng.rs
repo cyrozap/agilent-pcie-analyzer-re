f261aceb3ea1fd3e34c392bdf6d6ae3d154c7244
cyrozap <cyrozap@gmail.com>
2025-08-29 19:53:59 -0500

    Avoid unnecessary vector allocation in pad2pcapng
    We can use `write_all` to write data directly, without having to convert
    them to vectors first. Doing this increases processing speed by almost
    50%.

--- tools/agilent_pad/examples/pad2pcapng.rs
+++ tools/agilent_pad/examples/pad2pcapng.rs
@@ -2,7 +2,7 @@
 
 /*
  *  pad2pcapng.rs - Convert Agilent PAD files to PCAP-NG.
- *  Copyright (C) 2023-2024  Forest Crossman <cyrozap@gmail.com>
+ *  Copyright (C) 2023-2025  Forest Crossman <cyrozap@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -80,10 +80,10 @@ fn main() {
             .unwrap();
 
         let mut sh_data: Vec<u8> = Vec::new();
-        sh_data.append(&mut 0x1a2b3c4d_u32.to_le_bytes().to_vec());
-        sh_data.append(&mut 0x0001_u16.to_le_bytes().to_vec());
-        sh_data.append(&mut 0x0000_u16.to_le_bytes().to_vec());
-        sh_data.append(&mut (-1_i64).to_le_bytes().to_vec());
+        sh_data.write_all(&0x1a2b3c4d_u32.to_le_bytes()).unwrap();
+        sh_data.write_all(&0x0001_u16.to_le_bytes()).unwrap();
+        sh_data.write_all(&0x0000_u16.to_le_bytes()).unwrap();
+        sh_data.write_all(&(-1_i64).to_le_bytes()).unwrap();
 
         let sh_len: u32 = <usize as TryInto<u32>>::try_into(sh_data.len()).unwrap() + 4 * 3;
         pcapng_writer.write_all(&sh_len.to_le_bytes()).unwrap();
@@ -98,20 +98,21 @@ fn main() {
             .unwrap();
 
         let mut if_data: Vec<u8> = Vec::new();
-        if_data.append(&mut (147 + 11_u16).to_le_bytes().to_vec());
-        if_data.append(&mut 0x0000_u16.to_le_bytes().to_vec());
-        if_data.append(&mut 0_u32.to_le_bytes().to_vec());
+        if_data.write_all(&(147 + 11_u16).to_le_bytes()).unwrap();
+        if_data.write_all(&0x0000_u16.to_le_bytes()).unwrap();
+        if_data.write_all(&0_u32.to_le_bytes()).unwrap();
 
         // Options
-        let mut if_name = header.port_id.clone().into_bytes();
-        if_data.append(&mut 2_u16.to_le_bytes().to_vec());
-        if_data.append(
-            &mut <usize as TryInto<u16>>::try_into(if_name.len())
-                .unwrap()
-                .to_le_bytes()
-                .to_vec(),
-        );
-        if_data.append(&mut if_name);
+        let if_name = header.port_id.clone().into_bytes();
+        if_data.write_all(&2_u16.to_le_bytes()).unwrap();
+        if_data
+            .write_all(
+                &<usize as TryInto<u16>>::try_into(if_name.len())
+                    .unwrap()
+                    .to_le_bytes(),
+            )
+            .unwrap();
+        if_data.write_all(&if_name).unwrap();
         let padding_count = if if_data.len() % 4 != 0 {
             4 - (if_data.len() % 4)
         } else {
@@ -125,19 +126,20 @@ fn main() {
         if_data.append(&mut (2e9 as u64).to_le_bytes().to_vec());
         */
 
-        if_data.append(&mut 9_u16.to_le_bytes().to_vec());
-        if_data.append(&mut 1_u16.to_le_bytes().to_vec());
-        if_data.append(&mut 9_u32.to_le_bytes().to_vec());
+        if_data.write_all(&9_u16.to_le_bytes()).unwrap();
+        if_data.write_all(&1_u16.to_le_bytes()).unwrap();
+        if_data.write_all(&9_u32.to_le_bytes()).unwrap();
 
-        let mut if_hardware = header.module_type.clone().into_bytes();
-        if_data.append(&mut 15_u16.to_le_bytes().to_vec());
-        if_data.append(
-            &mut <usize as TryInto<u16>>::try_into(if_hardware.len())
-                .unwrap()
-                .to_le_bytes()
-                .to_vec(),
-        );
-        if_data.append(&mut if_hardware);
+        let if_hardware = header.module_type.clone().into_bytes();
+        if_data.write_all(&15_u16.to_le_bytes()).unwrap();
+        if_data
+            .write_all(
+                &<usize as TryInto<u16>>::try_into(if_hardware.len())
+                    .unwrap()
+                    .to_le_bytes(),
+            )
+            .unwrap();
+        if_data.write_all(&if_hardware).unwrap();
         let padding_count = if if_data.len() % 4 != 0 {
             4 - (if_data.len() % 4)
         } else {
@@ -145,8 +147,8 @@ fn main() {
         };
         if_data.resize(if_data.len() + padding_count, 0);
 
-        if_data.append(&mut 0_u16.to_le_bytes().to_vec());
-        if_data.append(&mut 0_u16.to_le_bytes().to_vec());
+        if_data.write_all(&0_u16.to_le_bytes()).unwrap();
+        if_data.write_all(&0_u16.to_le_bytes()).unwrap();
 
         let if_len: u32 = <usize as TryInto<u32>>::try_into(if_data.len()).unwrap() + 4 * 3;
         pcapng_writer.write_all(&if_len.to_le_bytes()).unwrap();
@@ -157,7 +159,7 @@ fn main() {
     for record in pad_file.records {
         assert_eq!(record.count, 1, "record \"count\" field is not equal to 1");
 
-        let mut record_data = pad_file.record_reader.get_all_data_for_record(&record);
+        let record_data = pad_file.record_reader.get_all_data_for_record(&record);
 
         // Enhanced Packet Block
         {
@@ -166,38 +168,46 @@ fn main() {
                 .unwrap();
 
             let mut block_data: Vec<u8> = Vec::with_capacity(4 * 1024);
-            block_data.append(&mut 0_u32.to_le_bytes().to_vec());
-            block_data.append(
-                &mut <u64 as TryInto<u32>>::try_into(record.timestamp_ns.checked_shr(32).unwrap())
-                    .unwrap()
-                    .to_le_bytes()
-                    .to_vec(),
-            );
-            block_data.append(
-                &mut <u64 as TryInto<u32>>::try_into(record.timestamp_ns & ((1 << 32) - 1))
-                    .unwrap()
-                    .to_le_bytes()
-                    .to_vec(),
-            );
+            block_data.write_all(&0_u32.to_le_bytes()).unwrap();
+            block_data
+                .write_all(
+                    &<u64 as TryInto<u32>>::try_into(record.timestamp_ns.checked_shr(32).unwrap())
+                        .unwrap()
+                        .to_le_bytes(),
+                )
+                .unwrap();
+            block_data
+                .write_all(
+                    &<u64 as TryInto<u32>>::try_into(record.timestamp_ns & ((1 << 32) - 1))
+                        .unwrap()
+                        .to_le_bytes(),
+                )
+                .unwrap();
             let record_data_len =
                 4 + 8 + 2 + 2 + 4 + <usize as TryInto<u32>>::try_into(record_data.len()).unwrap();
-            block_data.append(&mut record_data_len.to_le_bytes().to_vec());
-            block_data.append(&mut record_data_len.to_le_bytes().to_vec());
+            block_data
+                .write_all(&record_data_len.to_le_bytes())
+                .unwrap();
+            block_data
+                .write_all(&record_data_len.to_le_bytes())
+                .unwrap();
 
             // Record metadata
-            block_data.append(&mut record.number.to_le_bytes().to_vec());
-            block_data.append(&mut record.timestamp_ns.to_le_bytes().to_vec());
-            block_data.append(&mut record.lfsr.to_le_bytes().to_vec());
+            block_data.write_all(&record.number.to_le_bytes()).unwrap();
+            block_data
+                .write_all(&record.timestamp_ns.to_le_bytes())
+                .unwrap();
+            block_data.write_all(&record.lfsr.to_le_bytes()).unwrap();
             let value: u16 = if record.extra_metadata_present {
                 0x8000
             } else {
                 0
             } | record.metadata_offset;
-            block_data.append(&mut value.to_le_bytes().to_vec());
-            block_data.append(&mut record.flags.to_le_bytes().to_vec());
+            block_data.write_all(&value.to_le_bytes()).unwrap();
+            block_data.write_all(&record.flags.to_le_bytes()).unwrap();
 
             // Record data
-            block_data.append(&mut record_data);
+            block_data.write_all(&record_data).unwrap();
             let padding_count = if block_data.len() % 4 != 0 {
                 4 - (block_data.len() % 4)
             } else {
@@ -211,37 +221,37 @@ fn main() {
                 || (record.number == header.last_record_number
                     && header.trigger_record_number > header.last_record_number)
             {
-                let mut packet_comment =
-                    match header.timestamps_ns.trigger.cmp(&record.timestamp_ns) {
-                        Ordering::Less => {
-                            let difference_ns = record.timestamp_ns - header.timestamps_ns.trigger;
-                            let ts_ns_int = difference_ns / 1000000000;
-                            let ts_ns_frac = difference_ns % 1000000000;
-                            format!(
-                                "Triggered {}.{:09}s before this record.",
-                                ts_ns_int, ts_ns_frac
-                            )
-                        }
-                        Ordering::Equal => "Triggered on this record.".to_string(),
-                        Ordering::Greater => {
-                            let difference_ns = header.timestamps_ns.trigger - record.timestamp_ns;
-                            let ts_ns_int = difference_ns / 1000000000;
-                            let ts_ns_frac = difference_ns % 1000000000;
-                            format!(
-                                "Triggered {}.{:09}s after this record.",
-                                ts_ns_int, ts_ns_frac
-                            )
-                        }
+                let packet_comment = match header.timestamps_ns.trigger.cmp(&record.timestamp_ns) {
+                    Ordering::Less => {
+                        let difference_ns = record.timestamp_ns - header.timestamps_ns.trigger;
+                        let ts_ns_int = difference_ns / 1000000000;
+                        let ts_ns_frac = difference_ns % 1000000000;
+                        format!(
+                            "Triggered {}.{:09}s before this record.",
+                            ts_ns_int, ts_ns_frac
+                        )
                     }
-                    .into_bytes();
-                block_data.append(&mut 1_u16.to_le_bytes().to_vec());
-                block_data.append(
-                    &mut <usize as TryInto<u16>>::try_into(packet_comment.len())
-                        .unwrap()
-                        .to_le_bytes()
-                        .to_vec(),
-                );
-                block_data.append(&mut packet_comment);
+                    Ordering::Equal => "Triggered on this record.".to_string(),
+                    Ordering::Greater => {
+                        let difference_ns = header.timestamps_ns.trigger - record.timestamp_ns;
+                        let ts_ns_int = difference_ns / 1000000000;
+                        let ts_ns_frac = difference_ns % 1000000000;
+                        format!(
+                            "Triggered {}.{:09}s after this record.",
+                            ts_ns_int, ts_ns_frac
+                        )
+                    }
+                }
+                .into_bytes();
+                block_data.write_all(&1_u16.to_le_bytes()).unwrap();
+                block_data
+                    .write_all(
+                        &<usize as TryInto<u16>>::try_into(packet_comment.len())
+                            .unwrap()
+                            .to_le_bytes(),
+                    )
+                    .unwrap();
+                block_data.write_all(&packet_comment).unwrap();
                 let padding_count = if block_data.len() % 4 != 0 {
                     4 - (block_data.len() % 4)
                 } else {
@@ -249,8 +259,8 @@ fn main() {
                 };
                 block_data.resize(block_data.len() + padding_count, 0);
 
-                block_data.append(&mut 0_u16.to_le_bytes().to_vec());
-                block_data.append(&mut 0_u16.to_le_bytes().to_vec());
+                block_data.write_all(&0_u16.to_le_bytes()).unwrap();
+                block_data.write_all(&0_u16.to_le_bytes()).unwrap();
             }
 
             let block_len: u32 =
